6bee8a26a0e6b1305a0c68cf63dbc3ca064e8a15
Ian Michael <ianmichael.xp@gmail.com>
2020-09-08 18:13:30 -0300

    Material UI

--- src/components/FormularioCadastro.jsx
+++ src/components/FormularioCadastro.jsx
@@ -1,9 +1,23 @@
 import React from "react";
+import { Button, TextField } from "@material-ui/core";
 
 function FormularioCadastro() {
   return (
     <form>
-      <input type="text" />
+
+      <TextField id="nome" label="Nome" variant="outlined" />
+
+      <TextField id="sobrenome" label="Sobrenome" variant="outlined" />
+      <TextField id="cpf" label="CPF" variant="outlined" />
+      <label>Promoções</label>
+      <input type="checkbox" />
+
+      <label>Novidades</label>
+      <input type="checkbox" />
+
+      <Button variant="contained" color="primary">
+        Cadastrar
+      </Button>
     </form>
   );
 }
